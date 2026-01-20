03f58fa2f9bfe25fbd83c40f0f2246b1b7931b44
winner <eragbonikpeya@gmail.com>
2026-01-20 07:16:56 +0100

    remove cloudflare

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,6 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { nitro } from 'nitro/vite'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   resolve: {
@@ -16,7 +15,6 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
